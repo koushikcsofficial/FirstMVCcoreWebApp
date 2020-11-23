06c137558aa33d2a2ce3dcde8ccd338fcfd50ef1
KOUSHIK SAHA <koushik.official1999@gmail.com>
2020-11-23 12:24:02 +0530

    Converted anconsole applicatio to web application using .net core.

---
+++ FirstConsoleApp/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FirstConsoleApp
+{
+    public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment host)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endPoints =>
+                endPoints.MapGet("/", async context =>
+                    await context.Response.WriteAsync("Hello From Koushik")
+                )
+            );
+        }
+    }
+}
